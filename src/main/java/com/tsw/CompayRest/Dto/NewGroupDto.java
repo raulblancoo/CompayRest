1fbb1c170a4134a41fae4a70cff05c02cf234ac5
Raúl Blanco <64635695+raulblancoo@users.noreply.github.com>
2024-12-05 17:39:00 +0100

    Campos quitados porque no son necesarios

--- src/main/java/com/tsw/CompayRest/Dto/NewGroupDto.java
+++ src/main/java/com/tsw/CompayRest/Dto/NewGroupDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class NewGroupDto {
     private String group_name;
     private String currency;
-    private double amount;
-    private String imgURL;
+//    private double amount;
+//    private String imgURL;
     private List<String> userEmails;
 }
