94cd2fffd9b7faf0e2bcd5638929bd565ec9fc7f
Raúl Blanco <64635695+raulblancoo@users.noreply.github.com>
2025-01-12 13:32:38 +0100

    Responsive para tamaños pequeños

--- frontend/src/components/BizumsModal.js
+++ frontend/src/components/BizumsModal.js
@@ -3,18 +3,16 @@ import axiosInstance from "../components/axiosInstance";
 import { getCurrencySymbol } from "./CurrencyUtils";
 import { useTranslation } from "react-i18next";
 
-
 const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
-    const [currentUserId, setCurrentUserId] = useState(null); // Añadido para almacenar el userId del backend
+    const [currentUserId, setCurrentUserId] = useState(null); // Almacenar el userId del backend
     const { t } = useTranslation();
-
 
     // Obtener el userId desde el backend
     useEffect(() => {
         const fetchUserId = async () => {
             try {
-                const response = await axiosInstance.get("/users/me"); // Suponemos que este endpoint devuelve el usuario autenticado
-                setCurrentUserId(response.data.id); // Almacenamos el userId en el estado
+                const response = await axiosInstance.get("/users/me"); // Endpoint que devuelve el usuario autenticado
+                setCurrentUserId(response.data.id); // Almacenar el userId en el estado
             } catch (error) {
                 console.error("Error al obtener el userId:", error);
             }
@@ -25,11 +23,11 @@ const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
 
     if (!isOpen) return null;
 
-    // Si el `userId` aún no está cargado, mostramos un estado de carga o evitamos renderizar el contenido
+    // Mostrar un estado de carga si el `userId` aún no está cargado
     if (!currentUserId) {
         return (
             <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-                <p className="text-white text-lg">Cargando datos...</p>
+                <p className="text-white text-lg sm:text-xl">Cargando datos...</p>
             </div>
         );
     }
@@ -62,19 +60,19 @@ const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
     );
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-4xl max-h-[80vh] overflow-y-auto">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-2">
+            <div className="bg-white p-6 sm:p-8 rounded-lg shadow-lg w-full max-w-4xl max-h-[80vh] overflow-y-auto">
                 {/* Tabla de balances */}
                 <div className="mb-6">
-                    <h3 className="text-xl font-semibold mb-4 text-blue-600">{t("balances_by_user")}</h3>
+                    <h3 className="text-lg sm:text-xl font-semibold mb-4 text-blue-600">{t("balances_by_user")}</h3>
                     {balances.length > 0 ? (
-                        <table className="w-full text-left border-collapse">
+                        <table className="w-full text-left border-collapse text-xs sm:text-sm">
                             <thead>
                             <tr>
-                                <th className="border-b-2 py-2 uppercase">{t("user")}</th>
-                                <th className="border-b-2 py-2 text-green-500 uppercase">{t("is_owed")}</th>
-                                <th className="border-b-2 py-2 text-red-500 uppercase">{t("owes")}</th>
-                                <th className="border-b-2 py-2 text-gray-900 uppercase">{t("net_balance")}</th>
+                                <th className="border-b-2 py-2 uppercase px-2">{t("user")}</th>
+                                <th className="border-b-2 py-2 text-green-500 uppercase px-2">{t("is_owed")}</th>
+                                <th className="border-b-2 py-2 text-red-500 uppercase px-2">{t("owes")}</th>
+                                <th className="border-b-2 py-2 text-gray-900 uppercase px-2">{t("net_balance")}</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -89,22 +87,27 @@ const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
                                                 : "bg-gray-100"
                                     }
                                 >
-                                    <td className="ml-1 py-2 flex items-center">
+                                    <td className="py-2 px-2 flex items-center">
+                                        {/* Ocultar la imagen en pantallas pequeñas */}
                                         <img
                                             src={balance.avatarURL}
                                             alt={balance.name}
-                                            className="w-8 h-8 rounded-full mr-2"
+                                            className="w-6 h-6 sm:w-8 sm:h-8 rounded-full mr-2 hidden sm:block"
                                         />
-                                        {balance.name} {balance.surname}
+                                        {/* Mostrar solo el nombre en pantallas pequeñas */}
+                                        <span className="text-xs sm:text-sm">
+                                                {balance.name}{" "}
+                                            <span className="hidden sm:inline">{balance.surname}</span>
+                                            </span>
                                     </td>
-                                    <td className="py-2 text-gray-500 font-bold">
+                                    <td className="py-2 px-2 text-gray-500 sm:font-bold">
                                         {balance.received.toFixed(2)} {currencySymbol}
                                     </td>
-                                    <td className="py-2 text-gray-500 font-bold">
+                                    <td className="py-2 px-2 text-gray-500 sm:font-bold">
                                         {balance.paid.toFixed(2)} {currencySymbol}
                                     </td>
                                     <td
-                                        className={`py-2 font-bold ${
+                                        className={`py-2 px-2 font-bold ${
                                             balance.net > 0
                                                 ? "text-green-500"
                                                 : balance.net < 0
@@ -119,21 +122,22 @@ const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
                             </tbody>
                         </table>
                     ) : (
-                        <p className="text-gray-500 text-center">{t("noExpenses")}.</p>
+                        <p className="text-gray-500 text-center text-xs sm:text-sm">{t("noExpenses")}.</p>
                     )}
                 </div>
 
                 {/* Mis Bizums */}
                 <div className="mb-6">
-                    <h3 className="text-xl font-semibold mb-4 text-blue-600">{t("my_bizums")}</h3>
+                    <h3 className="text-lg sm:text-xl font-semibold mb-4 text-blue-600">{t("my_bizums")}</h3>
                     {userDebts.length > 0 ? (
-                        <ul className="space-y-2">
+                        <ul className="space-y-2 text-xs sm:text-sm">
                             {userDebts.map((bizum, index) => (
-                                <li key={index} className="p-4 bg-gray-100 rounded-md">
+                                <li key={index} className="p-3 sm:p-4 bg-gray-100 rounded-md">
                                     {bizum.loan_user.id === currentUserId ? (
                                         <p>
                                             <span className="font-semibold">
-                                                {bizum.payer_user.name} {bizum.payer_user.surname}
+                                                {bizum.payer_user.name}{" "}
+                                                <span className="hidden sm:inline">{bizum.payer_user.surname}</span>
                                             </span>{" "}
                                             {t("owes")}{" "}
                                             <span className="text-green-600">
@@ -148,7 +152,8 @@ const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
                                             </span>{" "}
                                             {t("to")}{" "}
                                             <span className="font-semibold">
-                                                {bizum.loan_user.name} {bizum.loan_user.surname}
+                                                {bizum.loan_user.name}{" "}
+                                                <span className="hidden sm:inline">{bizum.loan_user.surname}</span>
                                             </span>
                                         </p>
                                     )}
@@ -156,20 +161,21 @@ const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
                             ))}
                         </ul>
                     ) : (
-                        <p className="text-gray-500 text-center">{t("no_more_pending_actions")}</p>
+                        <p className="text-gray-500 text-center text-xs sm:text-sm">{t("no_more_pending_actions")}</p>
                     )}
                 </div>
 
                 {/* Otras Bizums */}
                 <div className="mb-6">
-                    <h3 className="text-xl font-semibold mb-4 text-blue-600">{t("other_bizums")}</h3>
+                    <h3 className="text-lg sm:text-xl font-semibold mb-4 text-blue-600">{t("other_bizums")}</h3>
                     {otherDebts.length > 0 ? (
-                        <ul className="space-y-2">
+                        <ul className="space-y-2 text-xs sm:text-sm">
                             {otherDebts.map((bizum, index) => (
-                                <li key={index} className="p-4 bg-gray-100 rounded-md">
+                                <li key={index} className="p-3 sm:p-4 bg-gray-100 rounded-md">
                                     <p>
                                         <span className="font-semibold">
-                                            {bizum.payer_user.name} {bizum.payer_user.surname}
+                                            {bizum.payer_user.name}{" "}
+                                            <span className="hidden sm:inline">{bizum.payer_user.surname}</span>
                                         </span>{" "}
                                         {t("owes")}{" "}
                                         <span className="text-red-600">
@@ -177,14 +183,15 @@ const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
                                         </span>{" "}
                                         {t("to")}{" "}
                                         <span className="font-semibold">
-                                            {bizum.loan_user.name} {bizum.loan_user.surname}
+                                            {bizum.loan_user.name}{" "}
+                                            <span className="hidden sm:inline">{bizum.loan_user.surname}</span>
                                         </span>
                                     </p>
                                 </li>
                             ))}
                         </ul>
                     ) : (
-                        <p className="text-gray-500 text-center">{t("no_more_pending_actions")}</p>
+                        <p className="text-gray-500 text-center text-xs sm:text-sm">{t("no_more_pending_actions")}</p>
                     )}
                 </div>
 
@@ -192,7 +199,7 @@ const BizumsModal = ({ isOpen, onClose, bizums, members, currency }) => {
                 <div className="flex justify-center">
                     <button
                         onClick={onClose}
-                        className="px-4 py-2 text-sm font-medium text-white bg-sky-500 hover:bg-cyan-700 rounded-lg"
+                        className="px-3 sm:px-4 py-2 sm:py-3 text-xs sm:text-sm font-medium text-white bg-sky-500 hover:bg-cyan-700 rounded-lg"
                     >
                         {t("close")}
                     </button>
